c18654fdb684c06b0c89f5f0c410ade1889c8047
JustRascal00 <Rascal.vamp00@gmail.com>
2024-09-03 18:16:25 +0400

    update: Authentication and Routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,40 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./routes/homepage/Homepage.jsx";
 import DashboardPage from "./routes/dashboard/DashboardPage.jsx";
 import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "./layouts/rootLayout/RootLayout.jsx";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout.jsx";
+import SignInPage from "./routes/SignInPage/SignInPage.jsx";
+import SignUpPage from "./routes/SignUpPage/SignUpPage.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/dashboard",
-
+    element: <RootLayout />,
     children: [
-      { path: "/dashboard", element: <DashboardPage /> },
-      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignInPage />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
     ],
   },
 ]);

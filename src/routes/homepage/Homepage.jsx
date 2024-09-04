a60bc33ba61c1d7df88babca694558a10f425101
JustRascal00 <Rascal.vamp00@gmail.com>
2024-09-04 18:02:26 +0400

    update: homepage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,7 +4,19 @@ import "./homepage.css";
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <div className="left">
+        <h1>CHATAI</h1>
+        <h2>Chat with AI</h2>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </div>
+      </div>
     </div>
   );
 };

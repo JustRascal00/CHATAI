55e784c6e33bd160ff91b5166617afde99ff736d
JustRascal00 <Rascal.vamp00@gmail.com>
2024-09-07 04:42:18 +0400

    update: chat and things

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,7 +1,30 @@
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 import "./chatPage.css";
 
 const ChatPage = () => {
-  return <div className="chatPage">ChatPage</div>;
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <NewPrompt />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;

d726d61c0a583d557e6d297a29031c929c650480
JustRascal00 <Rascal.vamp00@gmail.com>
2024-09-08 07:15:36 +0400

    update: database connect with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ImageKit from "imagekit";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,6 +12,14 @@ app.use(
   })
 );
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
   publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
@@ -23,5 +32,6 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.listen(port, () => {
+  connect();
   console.log("Server running on 3000");
 });
